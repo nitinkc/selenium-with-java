0ccac403fb5f2bbc9e8b49209892d2084139c73e
Nitin Chaurasia <gs.nitin@gmail.com>
2024-02-17 20:33:58 -0700

    final changes

---
+++ src/main/java/org/madhur/App.java
@@ -0,0 +1,21 @@
+package org.madhur;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://selenium.dev");
+        driver.quit();
+    }
+}
